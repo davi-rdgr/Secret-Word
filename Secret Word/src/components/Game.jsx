da9697e20ce50ba5b0a4dd0940e1952fb073337c
davi-rdgr <davirdgrsouza@gmail.com>
2024-02-17 21:53:37 -0300

    feat: style components aprimored enhanced

--- Secret Word/src/components/Game.jsx
+++ Secret Word/src/components/Game.jsx
@@ -17,15 +17,17 @@ const Game = ({ verifyLetter,
         setLetter("")
         letterInputRef.current.focus()
     }
+    
     return (
         <div className="game">
             <p className="points">
-                <span>Pontuação: {score}</span>
+                <span>Pontuação: <span className='score'>{score}</span></span>
             </p>
-            <h1>Adivinhe a palavra: </h1>
+            <h1>Secret Word <br />
+                Adivinhe a Palavra! </h1>   
 
-            <h3 className='tip'>Dica sobre a palavra: <span>{pickedCategoty}</span></h3>
-            <p>Você ainda tem {guesses} tentativas</p>
+            <h3 className='tip'>A dica é: <span>{pickedCategoty}</span></h3>
+            <p className='paragraph'>Você ainda tem {guesses} tentativas</p>
             <div className="wordContainer">
 
                 {leters.map((letter, i) => (
@@ -37,16 +39,16 @@ const Game = ({ verifyLetter,
                 ))}
             </div>
             <div className="letterContainer">
-                <p>Tente adivinhar uma letra da palavra: </p>
+                <p className='paragraph'>Tente adivinhar uma letra da palavra: </p>
                 <form onSubmit={handleSubmit}>
                     <input type="text" name='letter' maxLength={1} required onChange={(e) => setLetter(e.target.value)} value={letter} ref={letterInputRef} />
                     <button>Jogar!</button>
                 </form>
             </div>
             <div className="wrongLettersContainer">
-                <p>Letras já utilizadas: </p>
+                <p className='paragraph'>Letras já utilizadas: </p>
                 {wrongLetters.map((letter, i) => (
-                    <span key={i}>{letter}, </span>
+                    <span className='paragraph' key={i}>{letter}, </span>
                 ))}
             </div>
         </div>
